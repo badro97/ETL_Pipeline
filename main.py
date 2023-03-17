9b927ed8d9f81d545d6ea773bf99c2ff89792dbe
badro97 <sh97125@gmail.com>
2023-03-17 15:20:28 +0900

    데이터파티셔닝 2차 저장

--- main.py
+++ main.py
@@ -98,12 +98,12 @@ def ETL_Pipeline():
    
     ## Gzip을 사용한 데이터 압축/저장, .gz파일 업데이트 함수
     def GZIP(datafilepath, filename):
-        forPartition = []
+        # forPartition = []
         if not os.path.isfile(filename):
             with open(datafilepath, 'rb') as file_in:
                 with gzip.open(filename, 'wb') as file_out:        
                     file_out.writelines(file_in)
-            forPartition = gz_decoding(filename)
+            # forPartition = gz_decoding(filename)
         else:
             decoded_json = gz_decoding(filename)
             tmp_json = copy.deepcopy(decoded_json)
@@ -112,12 +112,12 @@ def ETL_Pipeline():
             for new in add_data:
                 if not is_duplicate(new, tmp_json): # 중복 데이터가 아닌 것들만 추가
                     tmp_json.append(new)
-                    forPartition.append(new)
+                    # forPartition.append(new)
             if tmp_json != decoded_json: # 새로 들어온 json 데이터가 있을 때만 .gz 파일 업데이트
                 with gzip.open(filename, 'wb') as f:
                     json_bytes = json.dumps(tmp_json).encode('utf-8')
                     f.write(json_bytes)
-        return forPartition
+        # return forPartition
 
 
     ## AWS S3 연결 (boto3)
@@ -137,33 +137,104 @@ def ETL_Pipeline():
             return s3
 
                    
-    ## AWS S3 파일 업로드
-    # def s3_File_Upload(filename, bucketname, loadname):
+    # ## AWS S3 파일 업로드
+    # def s3_File_Upload(filename, bucketname, s3_path):
     #     s3 = s3_connection()
     #     try:
-    #         s3.upload_file(filename, bucketname, loadname)
-    #         print(f'{loadname} upload complete!!')
+    #         s3.upload_file(filename, bucketname, s3_path)
     #     except Exception as e:
     #         print(e)
+    # s3_File_Upload(COMPRESSED_GZIP_PATH, AWS_S3_BUCKETNAME, AWS_S3_FILENAME)
     
     
-    ## AWS S3 파일 업로드 (데이터파티셔닝. 새로 들어온 데이터들을 처리한다.)
-    def s3_partitioning(forPartition):
+    
+    ## 데이터 파티셔닝 S3 적재 함수
+    def s3_upload_partitioning():
         s3 = s3_connection()
-        for log in forPartition:
+        all_data = gz_decoding(COMPRESSED_GZIP_PATH)
+        flag = all_data[0]['inDate'] # 실행될 때 마다 기준이 첫 데이터라 로그데이터가 많이 쌓이면 S3호출도 많아진다.
+                                     # 밖으로 빼서 처리하는 방법을 생각해보자.
+        
+        f_year = flag[:2]
+        f_month = flag[2:4]
+        f_day = flag[4:6]
+        f_hour = flag[6:8]
+        s3_path = f'data/20{f_year}_y/{f_month}_m/{f_day}_d/{f_hour}_h/'+AWS_S3_FILENAME
+        
+        for log in all_data:
             in_date_str = log['inDate']
             year = in_date_str[:2]
             month = in_date_str[2:4]
             day = in_date_str[4:6]
             hour = in_date_str[6:8]
-            s3_path = f'data/20{year}_y/{month}_m/{day}_d/{hour}_h/'+AWS_S3_FILENAME
+            if year!=f_year or month!=f_month or day!=f_day or hour!=f_hour: # 시간대별로 데이터가 나뉜다.
+                s3.upload_file('./log.gz', AWS_S3_BUCKETNAME, s3_path) # 시간이 달라지면 모아진 로그데이터 한번에 적재
+                print(f'{s3_path}\nS3 적재 완료')
+                f_year, f_month, f_day, f_hour = year, month, day, hour # 시간대 기준 바꿔주고
+                list(map(os.remove, ['./log.json', './log.gz'])) # 전 시간에 쌓여있던 로그들 삭제
+            else:
+                json_save('./log.json', [log]) # 시간이 바뀌지 않는다면 그냥 로그데이터를 쌓아놓는다
+                GZIP('./log.json', 'log.gz')
+    
+    
+    # ## AWS S3 파일 업로드 (데이터파티셔닝. 새로 들어온 데이터들을 처리한다.)
+    # def s3_partitioning(forPartition):
+    #     s3 = s3_connection()
+    #     update_data = []
+    #     for log in forPartition:
+    #         in_date_str = log['inDate']
+    #         year = in_date_str[:2]
+    #         month = in_date_str[2:4]
+    #         day = in_date_str[4:6]
+    #         hour = in_date_str[6:8]
+    #         s3_path = f'data/20{year}_y/{month}_m/{day}_d/{hour}_h/'+AWS_S3_FILENAME
             
-            json_save('./log.json', [log])  # 이 부분이 좀 꺼림칙하다.. 수정이 필요할 수도
-            GZIP('./log.json', 'log.gz')    # 새로운 데이터가 들어왔을 때 데이터가 정상적으로 추가되는지 확인할 것!!
-            s3.upload_file('./log.gz', AWS_S3_BUCKETNAME, s3_path)
+            
+    #         json_save('./log.json', [log])
+    #         GZIP('./log.json', 'log.gz')
+    #         s3.upload_file('./log.gz', AWS_S3_BUCKETNAME, s3_path)
+            
+            
+    #     ## 새로운 데이터 대처를 위해 사용 후 log 파일 삭제
+    #     list(map(os.remove, ['./log.json', './log.gz']))
+    
+    # ## AWS S3 파일 업로드 (데이터파티셔닝. 새로 들어온 데이터들을 처리한다.)
+    # def s3_partitioning(forPartition):
         
-        ## 새로운 데이터 대처를 위해 사용 후 log 파일 삭제
-        list(map(os.remove, ['./log.json', './log.gz']))
+    #     s3 = s3_connection()
+    #     s3.upload_file(filename, bucketname, loadname)
+        
+    #     for log in forPartition:
+    #         in_date_str = log['inDate']
+    #         year = in_date_str[:2]
+    #         month = in_date_str[2:4]
+    #         day = in_date_str[4:6]
+    #         hour = in_date_str[6:8]
+    #         s3_folder = f'data/20{year}_y/{month}_m/{day}_d/{hour}_h/'
+    #         s3_path = s3_folder+AWS_S3_FILENAME
+            
+    #         # 폴더가 없으면 생성
+    #         if not s3.list_objects_v2(Bucket=AWS_S3_BUCKETNAME, Prefix=s3_folder)['Contents']:
+    #             s3.put_object(Bucket=AWS_S3_BUCKETNAME, Key=s3_folder+'/')
+            
+    #         json_save('./log.json', [log])  # 이 부분이 좀 꺼림칙하다.. 수정이 필요할 수도
+    #         GZIP('./log.json', 'log.gz')    # 새로운 데이터가 들어왔을 때 데이터가 정상적으로 추가되는지 확인할 것!!
+    #         new_data = gz_decoding('./log.gz')
+    #         # s3.upload_file('./log.gz', AWS_S3_BUCKETNAME, s3_path)
+            
+            # 기존 파일 내용을 가져와서 JSON 객체로 변환
+            # response = s3.get_object(Bucket=AWS_S3_BUCKETNAME, Key=s3_path)
+            # data = response['Body'].read()
+            # old_data = gz_decoding(data)
+            
+            # update = old_data.extend(new_data)
+            # json_save('./update.json', update)
+            # GZIP('./update.json', 'update.gz')
+            # s3.upload_file('./update.gz', AWS_S3_BUCKETNAME, s3_path)
+            
+            
+    #     ## 새로운 데이터 대처를 위해 사용 후 log 파일 삭제
+    #     list(map(os.remove, ['./log.json', './log.gz']))
         
         
     # ## 데이터 파티셔닝을 위한 inDate(ISO 8601 형식) 파싱
@@ -181,25 +252,24 @@ def ETL_Pipeline():
     json_save(COMPRESSED_JSON_PATH, str_compressed_data)
     print('Extract(데이터 추출) Complete!')
     
-    forPartition = GZIP(COMPRESSED_JSON_PATH, COMPRESSED_GZIP_PATH)
-    # print(forPartition, len(forPartition))
+    GZIP(COMPRESSED_JSON_PATH, COMPRESSED_GZIP_PATH)
     print('Transform(데이터 변환) Complete!!')
     
-    ## 로직상 업로드 함수호출 제한 없이 해줘야 새로운 데이터 대응이 가능하다.
-    s3_partitioning(forPartition)       
+    s3_upload_partitioning()
     print('Load(데이터 저장) Complete!!!\n\n')
     
-    print(f'전체 json 데이터 길이: {len(gz_decoding(COMPRESSED_GZIP_PATH))}')
+    print(f'전체 json 데이터 길이: {len(gz_decoding(COMPRESSED_GZIP_PATH))}\n')
+    print(f'ETL_Pipeline 가동된지{round(len(gz_decoding(COMPRESSED_GZIP_PATH))/600, 2)}hour 경과\n\n')
     # if len(gz_decoding(COMPRESSED_GZIP_PATH)) % 100 == 0: 
     #     # s3_File_Upload(COMPRESSED_GZIP_PATH, AWS_S3_BUCKETNAME, AWS_S3_FILENAME) # S3 compressed.gz 파일 최신화
     #     print('Load(데이터 저장) Complete!!!\n\n')
     # else:
     #     print(f'gzip json user id 개수: {len(gz_decoding(COMPRESSED_GZIP_PATH))}')
 
-ETL_Pipeline()
+# ETL_Pipeline()
 
-# ## APscheduling, cron 표현식
-# scheduler = BlockingScheduler()
-# # 매일 13시부터 15시까지 5분 간격으로 실행
-# scheduler.add_job(ETL_Pipeline, 'cron', hour='13-15', minute='*/5') # crontab: */5 13-15 * * *
-# scheduler.start()
+## APscheduling, cron 표현식
+scheduler = BlockingScheduler()
+# 매일 13시부터 15시까지 5분 간격으로 실행
+scheduler.add_job(ETL_Pipeline, 'cron', hour='14-16', minute='*/10') # crontab: */5 13-15 * * *
+scheduler.start()
